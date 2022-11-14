de3dd90c35386c3a6526231719474fe5abd59f7c
mdanys <mochammaddany@gmail.com>
2022-11-14 23:47:39 +0700

    fix query get pendaki admin

--- config/config.go
+++ config/config.go
@@ -4,8 +4,7 @@ import (
 	"log"
 	"os"
 	"strconv"
-
-	"github.com/joho/godotenv"
+	// "github.com/joho/godotenv"
 )
 
 type AppConfig struct {
@@ -33,7 +32,7 @@ func NewConfig() *AppConfig {
 func initConfig() *AppConfig {
 	var app AppConfig
 
-	godotenv.Load("config.env")
+	// godotenv.Load("config.env")
 
 	serverPortConv, err := strconv.Atoi(os.Getenv("SERVER_PORT"))
 	if err != nil {
