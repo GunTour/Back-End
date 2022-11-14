db4458be516345c77a749069e51e41ca1964ba60
mdanys <mochammaddany@gmail.com>
2022-11-14 22:24:04 +0700

    fix query get pendaki admin

--- features/admin/repository/query.go
+++ features/admin/repository/query.go
@@ -29,7 +29,7 @@ func (rq *repoQuery) GetPendaki() ([]domain.BookingCore, domain.ClimberCore, err
 
 	if err := rq.db.Select("bookings.id_user", "users.full_name", "bookings.entrance", "bookings.date_start", "bookings.date_end").
 		Order("bookings.date_start asc").Joins("left join users on users.id = bookings.id_user").
-		Where("bookings.date_start > date_sub(now(), interval 2 week)").
+		Where("bookings.date_start < date_add(curdate(), interval 2 week)").
 		Find(&resQry).Scan(&resQry).Error; err != nil {
 		return nil, domain.ClimberCore{}, err
 	}
